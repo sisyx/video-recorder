360ae44a73bee1254019fc7791f019b544958977
sisyx <bahadorisajad908@gmail.com>
2024-02-13 01:24:16 +0330

    video recording complete

--- video.js
+++ video.js
@@ -1,72 +1,70 @@
 const vrecord = document.querySelector(".video-record-btn");
 const vpause = document.querySelector(".video-pause-btn");
-if (navigator.mediaDevices) {
-    console.log("getUserMedia supported.");
-  
-    const constraints = { audio: true };
-    let chunks = [];
-  
-    navigator.mediaDevices
-      .getUserMedia(constraints)
-      .then((stream) => {
-        const mediaRecorder = new MediaRecorder(stream);
-  
-        vrecord.onclick = () => {
-          mediaRecorder.start();
-          console.log(mediaRecorder.state);
-          console.log("recorder started");
-          record.style.background = "red";
-          record.style.color = "black";
-        };
-  
-        vpause.onclick = () => {
-          mediaRecorder.stop();
-          console.log(mediaRecorder.state);
-          console.log("recorder stopped");
-          record.style.background = "";
-          record.style.color = "";
-        };
-  
-        mediaRecorder.onstop = (e) => {
-          console.log("data available after MediaRecorder.stop() called.");
-  
-          const clipName = prompt("Enter a name for your sound clip");
-  
-          const clipContainer = document.createElement("article");
-          const clipLabel = document.createElement("p");
-          const audio = document.createElement("audio");
-          const deleteButton = document.createElement("button");
-          const mainContainer = document.querySelector("body");
-  
-          clipContainer.classList.add("clip");
-          audio.setAttribute("controls", "");
-          deleteButton.textContent = "Delete";
-          clipLabel.textContent = clipName;
-  
-          clipContainer.appendChild(audio);
-          clipContainer.appendChild(clipLabel);
-          clipContainer.appendChild(deleteButton);
-          mainContainer.appendChild(clipContainer);
-  
-          audio.controls = true;
-          const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
-          chunks = [];
-          const audioURL = URL.createObjectURL(blob);
-          audio.src = audioURL;
-          console.log("recorder stopped");
-  
-          deleteButton.onclick = (e) => {
-            const evtTgt = e.target;
-            evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-          };
-        };
-  
-        mediaRecorder.ondataavailable = (e) => {
-          chunks.push(e.data);
-        };
-      })
-      .catch((err) => {
-        console.error(`The following error occurred: ${err}`);
-      });
-  }
-  
+const videoElm = document.querySelector(".video-element");
+const finalVideoContainer = document.querySelector(".recorder-video-container");
+const finalVideo = document.querySelector(".final-video");
+const finalVideoUploadBtn = document.querySelector(".final-video-upload-btn");
+const finalVideoDeleteBtn = document.querySelector(".final-video-delete-btn");
+
+window.onload = async function () {
+    const videoStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+    })
+
+    const videoRecord = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+    })
+
+    videoElm.srcObject = videoStream
+
+    vrecord.addEventListener('click', () => {
+        var mediaRecorder = new MediaRecorder(videoRecord);
+
+        let blob = [];
+    
+        mediaRecorder.addEventListener("dataavailable", (event) => {
+            blob.push(event.data);
+        })
+    
+        mediaRecorder.addEventListener('stop', (event) => {
+            const videoLocal = URL.createObjectURL(new Blob(blob));
+            finalVideo.src = videoLocal;
+
+            finalVideoDeleteBtn.addEventListener("click", () => {
+                delete videoLocal;
+                finalVideo.src = "";
+                hideFinalVideo();
+            })
+
+        })
+    
+        mediaRecorder.start();
+        
+        vpause.addEventListener('click', () => {
+            mediaRecorder.stop()
+            enableRecord();
+            showFinalVideo();
+        })
+
+        disabelRecord();
+    })
+
+}
+
+function disabelRecord() {
+    vrecord.disabled = true;
+}
+
+function enableRecord() {
+    vrecord.disabled = false;
+}
+
+function showFinalVideo() {
+    finalVideoContainer.classList.remove('inactive');
+}
+
+function hideFinalVideo() {
+    finalVideoContainer.classList.add('inactive');
+}
