4aaef5f5356535100eb7ba668e9a7f7da76e4af6
sisyx <bahadorisajad908@gmail.com>
2024-02-17 04:13:02 +0330

    safari support added

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const startBtn = document.querySelector(".first-popup .btn");
 const startingPopup = document.querySelector('.first-popup')
 
 // video vars
-const vrecord = document.querySelector(".video-record-btn");
-const vpause = document.querySelector(".video-pause-btn");
-const videoElm = document.querySelector(".video-element");
-const finalVideoContainer = document.querySelector(".recorder-video-container");
-const finalVideo = document.querySelector(".final-video");
-const finalVideoUploadBtn = document.querySelector(".final-video-upload-btn");
-const finalVideoDeleteBtn = document.querySelector(".final-video-delete-btn");
+// const vrecord = document.querySelector(".video-record-btn");
+// const vpause = document.querySelector(".video-pause-btn");
+// const videoElm = document.querySelector(".video-element");
+// const finalVideoContainer = document.querySelector(".recorder-video-container");
+// const finalVideo = document.querySelector(".final-video");
+// const finalVideoUploadBtn = document.querySelector(".final-video-upload-btn");
+// const finalVideoDeleteBtn = document.querySelector(".final-video-delete-btn");
 
-let videoStream;
-let videoRecord;
-let mediaRecorder;
+// let videoStream;
+// let videoRecord;
+// let mediaRecorder;
 
 // voice vars
 const record = document.querySelector(".record");
@@ -41,7 +41,6 @@ function handleHeaderVoiceClick(event) {
     // change header's active button
     activeBtn.classList.remove("active");
     event.target.classList.add('active')
-    stopCamera();
     updateCanvaSize()
 }
 
@@ -50,7 +49,6 @@ function handleHeaderVideoClick(event) {
 }
 
 function showVideoPart() {
-    startCamera()
     const activeBtn = document.querySelector(".active");
 
     // show / hide elements
